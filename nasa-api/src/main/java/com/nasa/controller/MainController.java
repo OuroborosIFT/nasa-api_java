7f5773b1b5096beabb12f79dd2e4716e23c129c9
evy <jkb151103vare@gmail.com>
2024-05-12 09:06:51 +0300

    video display has been added

--- nasa-api/src/main/java/com/nasa/controller/MainController.java
+++ nasa-api/src/main/java/com/nasa/controller/MainController.java
@@ -2,6 +2,7 @@ package com.nasa.controller;
 
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,38 +11,42 @@ import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class MainController {
 
+    private final String NASA_URL = "https://api.nasa.gov/planetary/apod?api_key=enjSdOo36CZz7MOBlK29JUmsJ1Do8ya9LHhcQ0Jb";
+
 	@GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("url_map", content());
         return "index";
     }
-	
-	@GetMapping("/image")
-	public ModelAndView openImage() {
-		return new ModelAndView("redirect:" + imgUrl());
-	}
-
 
 	@SuppressWarnings("deprecation")
-    public URL imgUrl() {
+    public Map<String, URL> content() {
+        Map<String, URL> mapa = new HashMap<>();
+        String mediaType;
         URL url = null;
 
         try {
             HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.nasa.gov/planetary/apod?api_key=enjSdOo36CZz7MOBlK29JUmsJ1Do8ya9LHhcQ0Jb")).build();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create(NASA_URL)).build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             JSONObject json = new JSONObject(response.body());
 
-            url = new URL(json.getString("hdurl"));
+            mediaType = json.getString("media_type");
+            url = new URL(json.getString("url"));
+
+            mapa.put(mediaType, url);
+
         } catch (Exception e) {
-            e.printStackTrace();
+            e.fillInStackTrace();
         }
 
-        //assert url != null;
-        return url;
+        return mapa;
     }
 
 }
